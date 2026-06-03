7d30d39255c6b6275ec56f7a82d2c11d2a1bb7b5
Lufan Chang <502736353@qq.com>
2026-06-03 11:36:56 +0800

    Revert "简历分析、RAG、Agent Skill"
    This reverts commit 5e6b286055146c22c8c93198af3460ac0e78ed45.

--- scripts/start_frontend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d F:\PythonProject\easy-langent\frontend
-..\.venv\Scripts\python.exe -m http.server 5173 --bind 127.0.0.1 >> ..\logs\frontend.out.log 2>> ..\logs\frontend.err.log
--- scripts/start_local_dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d F:\PythonProject\easy-langent
-for /f "delims=" %%i in ('.venv\Scripts\python.exe -c "import certifi; print(certifi.where())"') do set SSL_CERT_FILE=%%i
-set PYTHONUTF8=1
-set PYTHONIOENCODING=utf-8
-.venv\Scripts\python.exe scripts\local_dev_server.py >> logs\local_dev.out.log 2>> logs\local_dev.err.log
